d58e2807dd5cf5f9bf32666f05e6e37000b130a3
Rafael Conquista <rafaelconquista2002@hotmail.com>
2023-09-20 23:45:32 -0300

    Update sketch.js

--- seminario/sketch.js
+++ seminario/sketch.js
@@ -1,28 +1,15 @@
-let ball; // Variável para representar a bola
-
-function setup() {
-  createCanvas(1400, 700); // Cria um canvas de 400x400 pixels
-  ball = new Ball(width / 2, -100, 20); // Inicializa a bola no topo do canvas
-}
-
-function draw() {
-  background(190); // Define o fundo do canvas
-
-  ball.applyGravity(); // Aplica a força da gravidade à bola
-  ball.update(); // Atualiza a posição da bola
-  ball.display(); // Exibe a bola
-}
-
-class Ball {
-  constructor(x, y, radius) {
+class FallingBall {
+  constructor(x, y, radius, mass, gravity) {
     this.position = createVector(x, y); // Posição inicial da bola
-    this.velocity = createVector(100, 0); // Velocidade inicial da bola
+    this.velocity = createVector(0, 0); // Velocidade inicial da bola
     this.radius = radius; // Raio da bola
-    this.gravity = createVector(0, 0.2); // Vetor de gravidade --> o primeiro parãmetro trata da gravidade no eixo y enquanto o segundo trata da gravidade no eixo x
+    this.mass = mass; // Massa da bola em quilogramas
+    this.gravity = gravity; // Aceleração da gravidade na Terra em m/s²
   }
 
   applyGravity() {
-    this.velocity.add(this.gravity); // Adiciona a força da gravidade à velocidade
+    const force = createVector(0, this.mass * this.gravity); // Calcula a força da gravidade (F = m * g)
+    this.velocity.add(force); // Adiciona a força à velocidade
   }
 
   update() {
@@ -30,15 +17,7 @@ class Ball {
     // Verifica se a bola atingiu o chão (no caso, a parte inferior do canvas)
     if (this.position.y > height - this.radius / 2) {
       this.position.y = height - this.radius / 2; // Mantém a bola no chão
-      this.velocity.y *= -0.8; // Inverte a velocidade vertical (rebote)
-    }
-    if (this.position.x > width - this.radius / 2) {
-        this.position.x = width - this.radius / 2; // Mantém a bola no chão
-        this.velocity.x *= -0.8; // Inverte a velocidade vertical (rebote)
-    }
-    if (this.position.x < 1 - this.radius / 2) {
-        this.position.x = 1 - this.radius / 2; // Mantém a bola no chão
-        this.velocity.x *= -0.8; // Inverte a velocidade vertical (rebote)
+      this.velocity.y *= -0.2; // Inverte a velocidade vertical (rebote)
     }
   }
 
@@ -47,4 +26,25 @@ class Ball {
     noStroke(); // Sem contorno
     ellipse(this.position.x, this.position.y, this.radius * 2); // Desenha a bola
   }
+}
+//exemplo de como instanciar o objeto e exibi-lo
+let ball; // Variável para representar a bola
+
+function setup() {
+  createCanvas(600, 650); // Cria um canvas de 400x400 pixels
+  ball = new FallingBall(width / 2, 0, 20, 1, 9.81); // Inicializa a bola no topo do canvas com massa 1 kg
+}
+
+function draw() {
+  background(220); // Define o fundo do canvas
+
+  ball.applyGravity(); // Aplica a força da gravidade à bola
+  ball.update(); // Atualiza a posição da bola
+  ball.display(); // Exibe a bola
+
+  // Calcula o peso da bola
+  const weight = ball.mass * ball.gravity;
+  fill(0);
+  textSize(16);
+  text(`Peso: ${weight.toFixed(2)} N`, 20, 20);
 }
