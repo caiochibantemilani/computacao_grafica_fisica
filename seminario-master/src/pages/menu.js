c35ea31040cb34d01eae5b1cd9d25f8b34767ac5
Caio Milani <67275692+caiochibantemilani@users.noreply.github.com>
2023-10-08 11:14:25 -0300

    Update menu.js

--- seminario-master/src/pages/menu.js
+++ seminario-master/src/pages/menu.js
@@ -9,7 +9,37 @@ function Menu(){
         <div className='all'>
         <img src={planets4k} alt="planets" className="tela"/>
             <div className="paginasFisica">
-            <div>
+            <div class="Sumario">
+                <h2 className='h2'>Física aplicadas em JavaScript</h2>
+                <hr className='.hr'></hr>
+                <li className="tema">
+                    <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
+                    <div className="legenda ">Sistema Solar</div>
+                </li>    
+                <li className="tema">
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/mola'}/> 
+                    <div className="legenda ">Simulação de Mola</div>
+                </li> 
+                <li className='tema'>
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/pendulum'}/> 
+                    <div className="legenda ">Simulação de pêndulo</div>
+                </li>
+                <li className='tema'>
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/colisao'}/> 
+                    <div className="legenda ">Simulação de colisão</div>
+                </li>
+                <li className='tema'>
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/cloth'}/> 
+                    <div className="legenda ">Cloth</div>
+                </li>
+                <li className='tema'>
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/NewtonCradle'}/> 
+                    <div className="legenda ">pêndulo de Newton</div>
+                </li>
+                <li className='tema'>
+                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/game'}/> 
+                    <div className="legenda ">Game</div>
+                </li>
                 <h2 className='h2'>Física aplicadas em JavaScript</h2>
                 <hr className='.hr'></hr>
                 <li className="tema">
