f3616a28d51859ccf7ed5a873bb4b7bc01379030
Caio Milani <67275692+caiochibantemilani@users.noreply.github.com>
2023-10-12 13:25:21 -0300

    arrumando erro da classe

--- seminario-master/src/pages/sistema_solar.js
+++ seminario-master/src/pages/sistema_solar.js
@@ -81,9 +81,9 @@ function SistemaSolar(anterior='') {
                     onMouseMove={handleMouseMove}
                     onWheel={handleWheel}
                 >
-                    <div classname="instrucoes">        
-                        <div classname="instrucao_esquerda">Use o mouse para se locomover pelo universo<span classname="material-symbols-outlined">pinch</span></div>
-                        <div classname="instrucao_direita">Para dar zoom use o scroll do mouse<span classname="material-symbols-outlined">zoom_in</span></div>
+                    <div className="instrucoes">        
+                        <div className="instrucao_esquerda">Use o mouse para se locomover pelo universo<span classname="material-symbols-outlined">pinch</span></div>
+                        <div className="instrucao_direita">Para dar zoom use o scroll do mouse<span classname="material-symbols-outlined">zoom_in</span></div>
                         </div>
                     <div id="sistema_solar_universo" className="sistema_solar">
                         <div id="sol" onClick={planeta_selecionado} className="sol">
