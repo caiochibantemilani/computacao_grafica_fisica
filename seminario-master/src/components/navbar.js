74a7b692ebac7aaff6a581b18c22f69c588f2bb3
Caio Milani <67275692+caiochibantemilani@users.noreply.github.com>
2023-10-14 23:15:05 -0300

    voltando

--- seminario-master/src/components/navbar.js
+++ seminario-master/src/components/navbar.js
@@ -18,47 +18,20 @@ function Navbar() {
                         Menu
                     </Button>
                 </div>
-                <div show={show} placement='end' onHide={handleClose}>
-                    <h2 class="h2">Física aplicadas em JavaScript</h2>
-                    <hr className='hr'></hr>
-                    <li className="tema">
-                        <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
-                        <div className="legenda ">Gravidade</div>
-                    </li>    
-                    <li className="tema">
-                        <img src={molaImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/mola'}/> 
-                        <div className="legenda ">Simulação de Mola</div>
-                    </li> 
-                    <li className='tema'>
-                        <img src={penduloImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/pendulum'}/> 
-                        <div className="legenda ">Simulação de pêndulo</div>
-                    </li>
-                    <li className='tema'>
-                        <img src={colisaoImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/colisao'}/> 
-                        <div className="legenda ">Simulação de colisão</div>
-                    </li>
-                    <li className='tema'>
-                        <img src={clothImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/cloth'}/> 
-                        <div className="legenda ">Cloth</div>
-                    </li>
-                    <li className='tema'>
-                        <img src={NewtonImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/NewtonCradle'}/> 
-                        <div className="legenda ">Pêndulo de Newton</div>
-                    </li>
-                    <li className='tema'>
-                        <img src={gameImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/game'}/> 
-                        <div className="legenda ">Game</div>
-                    </li>
-                    <li className='tema'>
-                        <img src={WaterImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/simulacao_agua'}/> 
-                        <div className="legenda ">Simulação de água</div>
-                    </li>
-                    <h2 class="h2">Bibliografia</h2>
-                    <li className="tema">
-                        <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
-                        <div className="legenda ">Gravidade</div>
-                    </li>
-                </div>
+                <Offcanvas show={show} placement='end' onHide={handleClose}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title>Física aplicadas em JavaScript</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        Some text as placeholder. In real life you can have the elements you
+                        have chosen. Like, text, images, lists, etc.
+                    </Offcanvas.Body>
+                    <Offcanvas.Title>Bibliografia</Offcanvas.Title>
+                    <Offcanvas.Body>
+                        Some text as placeholder. In real life you can have the elements you
+                        have chosen. Like, text, images, lists, etc.
+                    </Offcanvas.Body>
+                </Offcanvas>
             </nav>
         </div>
     );
