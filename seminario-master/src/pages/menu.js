bb23f7d00b77c5e54b52be7537ffd1f02f7b054e
Caio Milani <67275692+caiochibantemilani@users.noreply.github.com>
2023-10-16 22:36:07 -0300

    Atualização do Menu

--- seminario-master/src/pages/menu.js
+++ seminario-master/src/pages/menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import planets from '../images/sistema_solar_wallpaper.jpg'
 import planets4k from '../images/sistema_solar_wallpaper_4k.jpg'
 import molaImage from '../images/molaImage.png'
@@ -12,59 +13,118 @@ import WaterImage from '../images/water.png'
 import './menu.css'
 
 function Menu(){
+    const [clicado, setClicado] = useState(false);
+    const [evento, setEvento] = useState(null);
+    const [resumoGeral, setResumoGeral] = useState(null);
+
+    const [gravidade, setGravidade] = useState({
+        Titulo: 'Gravidade',
+        Resumo: 'PREENCHER COM O RESUMO DA GRAVIDADE',
+        Endereco: '/sistemaSolar'
+    });
+    const [mola, setMola] = useState({
+        Titulo: 'Mola',
+        Resumo: 'PREENCHER COM O RESUMO DA Mola',
+        Endereco: '/mola'
+    });
+    const [pendulo, setPendulo] = useState({
+        Titulo: 'Pendulo',
+        Resumo: 'PREENCHER COM O RESUMO DA Pendulo',
+        Endereco: '/pendulum'
+    });
+    const [colisao, setColisao] = useState({
+        Titulo: 'Colisao',
+        Resumo: 'PREENCHER COM O RESUMO DA Colisao',
+        Endereco: '/colisao'
+    });
+    const [cloth, setCloth] = useState({
+        Titulo: 'Cloth',
+        Resumo: 'PREENCHER COM O RESUMO DA Cloth',
+        Endereco: '/cloth'
+    });
+    const [newton, setNewton] = useState({
+        Titulo: 'Newton',
+        Resumo: 'PREENCHER COM O RESUMO DA Newton',
+        Endereco: '/NewtonCradle'
+    });
+    const [game, setGame] = useState({
+        Titulo: 'Game',
+        Resumo: 'PREENCHER COM O RESUMO DA Game',
+        Endereco: '/game'
+    });
+    const [agua, setAgua] = useState({
+        Titulo: 'Pendulo',
+        Resumo: 'PREENCHER COM O RESUMO DA Agua',
+        Endereco: '/simulacao_agua'
+    });
+
+    function planeta_selecionado(teste, resumo){
+        setClicado(true);
+        setEvento(teste);
+        setResumoGeral(resumo);
+    }
+
     return(
         <>
         <div className='all'>
         <Navbar/>
-        <img src={planets4k} alt="planets" className="tela"/>
+            {!clicado && (
+                <img src={planets4k} alt="planets" className="tela"/>
+            )}
+            {clicado && (
+                <img src={evento} alt="planets" className="tela"/>
+            )}
+            {clicado && (
+                <div className='resumo_geral'><h1>{resumoGeral.Titulo}</h1><p>{resumoGeral.Resumo}</p><a class="btn btn-primary" href={resumoGeral.Endereco}>Entrar</a></div>
+            )}
             <div className="paginasFisica">
             <div className="Sumario">
                 <h2 className='h2'>Física aplicadas em JavaScript</h2>
                 <hr className='hr'></hr>
                 <li className="tema">
-                    <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
+                    <img onClick={() => planeta_selecionado(planets, gravidade)}  src={planets} alt='vazio' className="tema_imagem" /> 
                     <div className="legenda ">Gravidade</div>
                 </li>    
                 <li className="tema">
-                    <img src={molaImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/mola'}/> 
+                    <img onClick={() => planeta_selecionado(molaImage, mola)} src={molaImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de Mola</div>
                 </li> 
                 <li className='tema'>
-                    <img src={penduloImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/pendulum'}/> 
+                    <img onClick={() => planeta_selecionado(penduloImage, pendulo)} src={penduloImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de pêndulo</div>
                 </li>
                 <li className='tema'>
-                    <img src={colisaoImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/colisao'}/> 
+                    <img onClick={() => planeta_selecionado(colisaoImage, colisao)} src={colisaoImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de colisão</div>
                 </li>
                 <li className='tema'>
-                    <img src={clothImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/cloth'}/> 
+                    <img onClick={() => planeta_selecionado(clothImage, cloth)} src={clothImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Cloth</div>
                 </li>
                 <li className='tema'>
-                    <img src={NewtonImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/NewtonCradle'}/> 
+                    <img onClick={() => planeta_selecionado(NewtonImage, newton)} src={NewtonImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Pêndulo de Newton</div>
                 </li>
                 <li className='tema'>
-                    <img src={gameImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/game'}/> 
+                    <img onClick={() => planeta_selecionado(gameImage, game)} src={gameImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Game</div>
                 </li>
                 <li className='tema'>
-                    <img src={WaterImage} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/simulacao_agua'}/> 
+                    <img onClick={() => planeta_selecionado(WaterImage, agua)} src={WaterImage} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de água</div>
                 </li>
                 <h2 className='h2'>Bibliografia</h2>
                 <hr className='hr'></hr>
                 <li className="tema">
-                    <img src={planets} alt='vazio' className="tema_imagem" onClick={() => window.location.href = '/sistemaSolar'}/> 
+                    <img src={planets} alt='vazio' className="tema_imagem"/> 
                     <div className="legenda ">Sistema Solar</div>
                 </li>    
                 <li className="tema">
-                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/mola'}/> 
+                    <img src={planets} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de Mola</div>
                 </li> 
                 <li className='tema'>
-                    <img src={planets} className="tema_imagem" alt='vazio' onClick={() => window.location.href = '/pendulum'}/> 
+                    <img src={planets} className="tema_imagem" alt='vazio'/> 
                     <div className="legenda ">Simulação de pêndulo</div>
                 </li>
                 <li className='tema'>
